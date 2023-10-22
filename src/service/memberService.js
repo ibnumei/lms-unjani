4a9aa1ab331a6d172d39f675a27784546f7399ca
Hayuning Azaniarti <hayuning.nia@BLUEBIRDGROUP.COM>
2023-10-23 00:29:18 +0700

    remove comment code

--- src/service/memberService.js
+++ src/service/memberService.js
@@ -27,36 +27,24 @@ class UserService {
   // }
 
   static async syncMember(transaction){
-    // const currentPage = 1
-    // const maxPage = 10
-    // for(currentPage; currentPage <= maxPage; currentPage++) {
-    //     const result = await axios.get('http://library-lama.unjani.id/index.php?p=api/member/1/000SSFNNSA00124')
-    //     const dataMember  =  result.data.data
-    //     const attributes = ['id', 'member_id', 'member_name', 'gender', 'member_type_name', 'member_mail_address', 'member_email', 'member_image', 'pin', 'member_since_date', 'register_date', 'expire_date', 'member_notes', 'input_date', 'last_update','isActive','createdBy', 'createdDate'];
-    //     dataMember.map(singleData =>  {
-    //       singleData.isActive = true
-    //       singleData.createdBy = "SYSTEM";
-    //       singleData.createdDate = new Date();
-    //       return singleData
-    //     })
-    //     await bulkInsertUpdate(memberBean, dataMember, attributes, transaction)
-    // }
-    try {
-      const result = await axios.get('http://library-lama.unjani.id/index.php?p=api/member/1/000SSFNNSA00124');
-      const dataMember  =  result.data.data;
-      const attributes = ['id', 'member_id', 'member_name', 'gender', 'member_type_name', 'member_mail_address', 'member_email', 'member_image', 'pin', 'member_since_date', 'register_date', 'expire_date', 'member_notes', 'input_date', 'last_update','isActive','createdBy', 'createdDate'];
-      dataMember.map(singleData =>  {
-        singleData.isActive = true;
-        singleData.createdBy = "SYSTEM";
-        singleData.createdDate = new Date();
-        return singleData;
-      })
-      await bulkInsertUpdate(memberBean, dataMember, attributes, transaction);
-      return true;
-    } catch(e) {
-      throw new Error('Failed Sync Data', e);
+    let currentPage = 901
+    let maxPage = 1000
+    const gettersPromise = []
+    for(currentPage; currentPage <= maxPage; currentPage++) {
+      console.log('currentPage', currentPage)
+        gettersPromise.push(axios.get(`http://library-lama.unjani.id/index.php?p=api/member/${currentPage}/000SSFNNSA00124`))
     }
-        
+        const resultPromise = await Promise.all(gettersPromise);
+        let member = []
+        resultPromise.forEach((response) => {
+          member = [
+            ...member,
+            ...response.data.data
+          ]
+        })
+        const attributes = ['id', 'member_id', 'member_name', 'gender', 'member_type_name', 'member_mail_address', 'member_email', 'member_image', 'pin', 'member_since_date', 'register_date', 'expire_date', 'member_notes', 'input_date', 'last_update','isActive','createdBy', 'createdDate'];
+        await bulkInsertUpdate(memberBean, member, attributes, transaction)
+        return;
   }
 }
 
