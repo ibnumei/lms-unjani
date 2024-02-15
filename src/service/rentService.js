7e19bad61e4085cb3dbce0c99ec415a95fb67f86
Arya Wiranata <wiranata.arya.wiranata@gmail.com>
2024-02-15 23:08:24 +0700

    add post old API Library for loaning

--- src/service/rentService.js
+++ src/service/rentService.js
@@ -1,13 +1,16 @@
+const axios = require('axios');
+const FormData = require('form-data');
 const { rentDao } = require('../dao/index');
 const { v4: uuidv4 } = require('uuid');
-const {clone} = require('../util/ServerTool');
+const { clone } = require('../util/ServerTool');
 
+const API_ROUTE = (type) => `https://deltalibrary.unjani.id/index.php?p=api/loan/${type}`
 
 class RentService {
   static async searchRentBook(whereBook, whereItems, transaction) {
     let result = await rentDao.searchRentBook(whereBook, whereItems, transaction);
     let tempAuthor = '';
-    if(Object.keys(result.authors).length > 0)  {
+    if (Object.keys(result.authors).length > 0) {
       result.authors.forEach((author) => {
         tempAuthor = tempAuthor + `${author.author_name} `;
       })
@@ -18,13 +21,13 @@ class RentService {
 
   static async rentBook(payload, currentUser, transaction) {
     const uuid = uuidv4();
-    if(payload.length > 2) {
+    if (payload.length > 2) {
       throw new Error('Peminjaman tidak boleh lebih dari 2 buku');
     }
     const newPayload = [
       {
         kode_pinjam: uuid,
-        id_member:  currentUser.id,
+        id_member: currentUser.id,
         id_book: payload[0].id_book,
         item_code: payload[0].item_code,
         tgl_pinjam: new Date(),
@@ -41,19 +44,33 @@ class RentService {
       //   createdBy: currentUser.member_name
       // }
     ]
-    if(payload.length > 1) {
+
+    const form = new FormData()
+    form.append('member_key', currentUser.member_id)
+    form.append('item_code[]', payload[0].item_code)
+
+    if (payload.length > 1) {
       const secondPayload = {
-          kode_pinjam: uuid,
-          id_member:  currentUser.id,
-          id_book: payload[1].id_book,
-          item_code: payload[1].item_code,
-          tgl_pinjam: new Date(),
-          status_pinjam: true,
-          createdBy: currentUser.member_name
-        }
+        kode_pinjam: uuid,
+        id_member: currentUser.id,
+        id_book: payload[1].id_book,
+        item_code: payload[1].item_code,
+        tgl_pinjam: new Date(),
+        status_pinjam: true,
+        createdBy: currentUser.member_name
+      }
       newPayload.push(secondPayload)
+
+      form.append('item_code[]', payload[1].item_code)
     }
     await rentDao.rentBook(newPayload, transaction);
+
+    const formHeaders = form.getHeaders();
+    await axios.post(API_ROUTE('loaning'), form, {
+      headers: {
+        ...formHeaders
+      }
+    })
     const type = 'rent';
     await this.updateItems(payload, type, transaction);
     return uuid;
@@ -67,7 +84,7 @@ class RentService {
     }
     const attributes = ['kode_pinjam', 'item_code'];
     const dataRentBook = await rentDao.searchRentData(where, transaction, attributes);
-    if(!dataRentBook.length) {
+    if (!dataRentBook.length) {
       throw new Error('Data Tidak ditemukan atau status sudah dikembalikan');
     }
     const type = 'return'
@@ -83,11 +100,13 @@ class RentService {
     const itemBook = await rentDao.searchItems(itemsCode)
     const newItemBook = clone(itemBook)
     const statements = []
-    newItemBook.forEach((data)=> {
+    newItemBook.forEach((data) => {
       const newStock = type === 'rent' ? data.stock - 1 : data.stock + 1
       statements.push(rentDao.updateItems(newStock, data.item_code, transaction))
     })
     await Promise.all(statements);
+
+    return
   }
 
   static async searchReturnBook(kode_pinjam, transaction) {
@@ -97,12 +116,12 @@ class RentService {
     }
     const attributes = ['kode_pinjam', 'item_code', 'id_book'];
     const dataRent = await rentDao.searchRentData(where, transaction, attributes);
-    if(!dataRent.length) {
+    if (!dataRent.length) {
       throw new Error('Data Tidak ditemukan atau status sudah dikembalikan');
     }
     const book = []
     for (const file of dataRent) {
-      const whereBook =  { id_book: file.id_book };
+      const whereBook = { id_book: file.id_book };
       const whereItems = { item_code: file.item_code };
       const tempBook = await this.searchRentBook(whereBook, whereItems, transaction);
       book.push(tempBook)
@@ -124,15 +143,15 @@ class RentService {
   static getPagination(page, size) {
     const limit = size ? +size : 10;
     const offset = page ? page * limit : 0;
-  
+
     return { limit, offset };
   };
 
-  static getPagingData (data, page, limit) {
+  static getPagingData(data, page, limit) {
     const { count: totalItems, rows: transactions } = data;
     const currentPage = page ? +page : 0;
     const totalPages = Math.ceil(totalItems / limit);
-  
+
     // return { totalItems, transactions, totalPages, currentPage };
     return data
   };
